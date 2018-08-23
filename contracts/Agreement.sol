5d2432811157741b4271392ad9d869285ceb5f18
Paul Worrall <paul.worrall@interition.net>
2018-08-23 16:18:46 +0100

    started fleshing out tests wit implementation

--- contracts/Agreement.sol
+++ contracts/Agreement.sol
@@ -6,10 +6,15 @@ contract Agreement {
     address private _PlacingCounterParty;
     address private _AcceptingCounterParty;
 
+    address private _Beneficiary;
+
+    bool private _Determined;
+
     string private _subject;
     bool private _accepted = false;
 
     event Accepted(address indexed acceptingCounterParty, string subject);
+    event Determined(address indexed determiningCounterParty);
 
     constructor(string subject, address acceptingCounterParty) public {
         _PlacingCounterParty = msg.sender;
@@ -17,7 +22,7 @@ contract Agreement {
         _AcceptingCounterParty = acceptingCounterParty;
     }
 
-    function accept() public {
+    function setAccepted() public {
         require(msg.sender == _AcceptingCounterParty);
         _accepted = true;
         emit Accepted(msg.sender,_subject);
@@ -30,6 +35,23 @@ contract Agreement {
     function isAccepted() public view returns(bool) {
         return _accepted;
     }
+
+    function setBeneficiary() public {
+        require( msg.sender == _PlacingCounterParty || msg.sender == _AcceptingCounterParty);
+
+        require( _Determined == false );
+
+        _Beneficiary = msg.sender;
+
+        _Determined = true;
+
+        emit Determined(msg.sender);
+    }
+
+    function getBeneficiary() public view returns(address) {
+        return _Beneficiary;
+    }
+
 
 
 }
--- test/TestAgreement.js
+++ test/TestAgreement.js
@@ -1,11 +1,69 @@
 var Agreement = artifacts.require("Agreement");
 
 contract('Agreement', function(accounts) {
-    it("should be TBD for the agreement subject", function() {
+    it("should be TBD for the Agreement Subject", function() {
         return Agreement.deployed().then(function(instance) {
             return instance.getSubject.call();
         }).then(function(subject) {
             assert.equal(subject, "TBD", "subject was not TBD");
         });
     });
+
+    it("should be 'not accepted' at Origination", function() {
+        return Agreement.deployed().then(function(instance) {
+            return instance.isAccepted.call();
+        }).then(function(accepted) {
+            assert.isFalse(accepted, "was accepted at origination");
+        });
+    });
+
+    it("should provide a receipt after Taker accepts", function() {
+        return Agreement.deployed().then(function(instance) {
+            return instance.setAccepted({from: accounts[1]});
+        }).then(function(result) {
+            //console.log(JSON.stringify(result));
+            assert.isOk(result.receipt, "receipt was not provided");
+        });
+    });
+
+
+    it("should report Accepted as true when Taker has accepted", function() {
+        return Agreement.deployed().then(function(instance) {
+            return instance.isAccepted.call();
+        }).then(function(accepted) {
+            assert.isTrue(accepted, "did not report Taker had Accepted");
+        })
+    });
+
+
+    it("should provide a receipt when Originator sets as the Beneficiary", function() {
+        return Agreement.deployed().then(function(instance) {
+            return instance.setBeneficiary({from: accounts[0]});
+        }).then(function(result) {
+            assert.isOk(result.receipt, "receipt was not provided");
+        })
+    });
+
+    it("should report that Originator is the Beneficiary", function() {
+        return Agreement.deployed().then(function(instance) {
+            return instance.getBeneficiary.call();
+        }).then(function(address) {
+            assert.equal(accounts[0],address, "Originator was not Beneficiary");
+        })
+    });
+
+    // not sure how to test this behaviour properly because how do you handle a throw/revert
+    it("should not be able to set Beneficiary if already set", function() {
+        return Agreement.deployed().then(function (instance) {
+            return instance.setBeneficiary({from: accounts[0]});
+        }).then(function () {
+           assert(false,"should have error with throw or revert");
+        }).catch(function (err) {
+            assert.equal(err, "Error: VM Exception while processing transaction: revert", "did not throw or revert as expected");
+        });
+    });
+
+    // "should only allow Layer and Taker to Determine outcome"
+
+
 });
