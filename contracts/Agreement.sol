bf0f8031206c0d523e3a3f034ee6c3648b202ccb
Paul Worrall <paul.worrall@interition.net>
2018-08-23 20:19:20 +0100

    doc remarks

--- contracts/Agreement.sol
+++ contracts/Agreement.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.22;
 
 /**
  * @title Agreement
+ * @author Paul Worrall
+ * @notice This is part of the DistPute Smart Contract framework
  * @dev First stab at an Agreement between two parties with dispute resolution
  */
 
@@ -16,7 +18,7 @@ contract Agreement {
     bool private _Determined = false;
     bool private _Disputed = false;
 
-    // for testing the subject is a trivial text statement but will be a Contract adhering to an Interface standard
+    /// for testing the subject is a trivial text statement but will be a Contract adhering to an Interface standard
     string private _Subject;
     bool private _Accepted = false;
 
@@ -52,8 +54,8 @@ contract Agreement {
     */
     event Settled(address indexed beneficiary, address indexed adjudicator);
 
-    // this Contract would be produced by a Factory that would ensure the Adjudicator address is safely supplied
-    // the adjudicator could be a Contract address of a Multi-Sig wallet of course
+    /// this Contract would be produced by a Factory that would ensure the Adjudicator address is safely supplied
+    /// the adjudicator could be a Contract address of a Multi-Sig wallet of course
     constructor(string subject, address taker, address adjudicator) public {
         _Placer = msg.sender;
         _Subject = subject;
@@ -67,7 +69,7 @@ contract Agreement {
         emit Accepted(msg.sender,_Subject);
     }
 
-    // @return true if crowdsale event has ended
+    /// @return true if crowdsale event has ended
     function getSubject() public view returns(string) {
         return _Subject;
     }
@@ -80,7 +82,7 @@ contract Agreement {
         return _Disputed;
     }
 
-    // either party may claim to be the beneficiary
+    /// either party may claim to be the beneficiary
 
     function setBeneficiary() public {
 
@@ -99,7 +101,7 @@ contract Agreement {
         return _Beneficiary;
     }
 
-    // either the placer or the taker can raise a dispute
+    /// either the placer or the taker can raise a dispute
 
     function setDispute() public {
 
@@ -113,8 +115,8 @@ contract Agreement {
         emit Dispute(msg.sender);
     }
 
-    // adjudicator settles the contract in one parties favour
-    // if Adjudicator is multi-sig wallet/Adjudicator registry Contract this will be called from that
+    /// adjudicator settles the contract in one parties favour
+    /// if Adjudicator is multi-sig wallet/Adjudicator registry Contract this will be called from that
     function setFavour(address favoured) public {
         require( msg.sender == _Adjudicator );
 
@@ -127,42 +129,42 @@ contract Agreement {
 
     }
 
-    // causes the settlement [separate set of contracts]
+    /// causes the settlement [separate set of contracts]
 
     function settle() public {
 
         require( msg.sender == _Placer || msg.sender == _Taker);
 
         if(_Disputed == true) {
-            // fees payable to adjudicator
+            /// fees payable to adjudicator
         }
 
-        // After satisfying all criteria go ahead and pay fees and settle to the beneficiary
+        /// After satisfying all criteria go ahead and pay fees and settle to the beneficiary
 
-        // settlement needs to come from the parties from their mutual agreement or from adjudication
+        /// settlement needs to come from the parties from their mutual agreement or from adjudication
 
 
         emit Settled(_Beneficiary,_Adjudicator); // although _Adjudicator might not have been used
     }
 
-    // cancellation needs to be agreed by both parties and, if an adjudicator was used, will still pay their fees
+    /// cancellation needs to be agreed by both parties and, if an adjudicator was used, will still pay their fees
 
     function cancel() public view {
 
         require( msg.sender == _Placer || msg.sender == _Taker);
 
-        // add sender to list
+        /// add sender to list
 
-        // if list has both parties ...
+        /// if list has both parties ...
 
         if(_Disputed == true) {
-            // fees payable to adjudicator
+            /// fees payable to adjudicator
         }
 
-        // return balance to parties
+        /// return balance to parties
 
 
-        // kill contract
+        /// kill contract
     }
     
 
